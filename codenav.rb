2344a2370b670232818225edda84bfaa4a23d3af
Hasnae <hasnae@viqueen.org>
2021-12-28 10:55:37 +1100

    initial 2.0.0

---
+++ codenav.rb
@@ -0,0 +1,19 @@
+class Codenav < Formula
+  desc "Easing up code navigation between repos"
+  homepage "https://github.com/viqueen/codenav"
+  url "https://github.com/viqueen/codenav/archive/refs/tags/2.0.0.tar.gz"
+  sha256 "07a7f0c3894a5baa6fcaed4193a0e698acc934731aa183713b2c173907f2b2a3"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "run", "ci:build"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    raise "test not implemented"
+  end
+end
